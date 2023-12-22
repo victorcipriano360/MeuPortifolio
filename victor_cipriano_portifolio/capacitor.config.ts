271c0c6be646e241db1f814889fa8f10fa2872e3
victorcipriano360 <125392545+victorcipriano360@users.noreply.github.com>
2023-12-22 14:03:18 -0300

    Projeto Blank em branco

---
+++ victor_cipriano_portifolio/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'victor_cipriano_portifolio',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
